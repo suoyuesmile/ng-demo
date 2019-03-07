c6afae277f4fd5887fa5a703f3e5af545f81869e
shaosuo <shaosuo@youmi.net>
2019-03-07 11:49:36 +0800

    补充代码

--- ng-app/src/app/heroes/heroes.component.ts
+++ ng-app/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service'
-// import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +8,7 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
   heroes: Hero[]
-  selectedHero: Hero
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-  }
   getHeroes(): void {
     this.heroServive.getHeroes().subscribe(heroes => (this.heroes = heroes))
   }
